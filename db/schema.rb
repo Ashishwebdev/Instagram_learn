a66f37e6d4945ad274e18b513dc7d7c91646bd5c
singhashish19031998@gmail.com <singhashish19031998@gmail.com>
2017-06-01 11:49:22 +0530

    Bootstrap template used

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601050941) do
+ActiveRecord::Schema.define(version: 20170601054822) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
